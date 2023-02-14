fa0c6ff1325fff00705af173bbc951cb115ddc6b
skrmnyi <carhatt.xm@gmail.com>
2023-02-14 19:41:10 +0100

    save progress

--- src/main/java/QA/Exception/Exception.java
+++ src/main/java/QA/Exception/Exception.java
@@ -1,7 +1,10 @@
 package QA.Exception;
 
 import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class Exception {
   void devider(int numbers[], int deviderValue) {
     int devidedResults[] = new int[numbers.length];
     try {
@@ -9,11 +12,11 @@ import java.util.Arrays;
         int result = numbers[i] / deviderValue;
         devidedResults[i] = result;
       }
+      System.out.println(Arrays.toString(devidedResults));
     } catch (ArithmeticException exception) {
       System.out.println("Back to school and learn math");
     }
   }
-
 
   public static void main(String[] args) {
     Scanner userInput = new Scanner(System.in);
